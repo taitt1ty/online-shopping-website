552c8859581ba97f711b1b375702e9f0ce3ea654
HuongPham01 <150899326+huongpham01@users.noreply.github.com>
2024-05-09 15:37:08 +0700

    refactor: response shop cart

--- backend/src/services/shopCartService.js
+++ backend/src/services/shopCartService.js
@@ -1,115 +1,74 @@
 import db from "../models/index";
-import { successResponse, errorResponse } from "../utils/ResponseUtils";
+import {
+  successResponse,
+  errorResponse,
+  missingRequiredParams,
+} from "../utils/ResponseUtils";
 
 const addShopCart = async (data) => {
   try {
     if (!data.userId || !data.sizeId || !data.quantity) {
-      return errorResponse("Missing required parameter!");
+      return missingRequiredParams("user, size, quantity are");
     }
-
+    // Function to calculate available stock
+    const calculateStock = async (sizeId) => {
+      let receiptDetail = await db.ReceiptDetail.findAll({ where: { sizeId } });
+      let orderDetail = await db.OrderDetail.findAll({
+        where: { productId: sizeId },
+      });
+      let quantity = 0;
+      for (let j = 0; j < receiptDetail.length; j++) {
+        quantity += receiptDetail[j].quantity;
+      }
+      for (let k = 0; k < orderDetail.length; k++) {
+        let order = await db.Order.findOne({
+          where: { id: orderDetail[k].orderId },
+        });
+        if (order.statusId != "S7") {
+          quantity -= orderDetail[k].quantity;
+        }
+      }
+      return quantity;
+    };
+    // Check if the cart already exists
     const cart = await db.ShopCart.findOne({
-      where: {
-        userId: data.userId,
-        sizeId: data.sizeId,
-        statusId: 0,
-      },
+      where: { userId: data.userId, sizeId: data.sizeId, statusId: 0 },
       raw: false,
     });
-
+    // Check stock availability
+    const stock = await calculateStock(data.sizeId);
     if (cart) {
-      // Find the product's inventory quantity
-      let res = await db.ProductSize.findOne({
-        where: { id: data.sizeId },
-      });
-      if (res) {
-        // Calculate inventory quantity based on invoice and purchase order details
-        /*
-        let receiptDetail = await db.ReceiptDetail.findAll({
-          where: { sizeId: res.id },
-        });
-        let orderDetail = await db.OrderDetail.findAll({
-          where: { productId: res.id },
-        });
-        let quantity = 0;
-        for (let j = 0; j < receiptDetail.length; j++) {
-          quantity += receiptDetail[j].quantity;
-        }
-        for (let k = 0; k < orderDetail.length; k++) {
-          let order = await db.Order.findOne({
-            where: { id: orderDetail[k].orderId },
-          });
-          // Do not calculate quantities from unfulfilled orders
-          if (order.statusId != "S7") {
-            quantity -= orderDetail[k].quantity;
-          }
-        }
-        res.stock = quantity;
-        */
-      }
-
-      // Update or add new products to the cart
       if (data.type === "UPDATE_QUANTITY") {
-        if (+data.quantity > res.stock) {
-          return errorResponse(`Chỉ còn ${res.stock} sản phẩm`, 2, res.stock);
+        if (+data.quantity > stock) {
+          return errorResponse(`Chỉ còn ${stock} sản phẩm`, 2, stock);
         } else {
           cart.quantity = +data.quantity;
           await cart.save();
         }
       } else {
-        if (+cart.quantity + +data.quantity > res.stock) {
-          return errorResponse(`Chỉ còn ${res.stock} sản phẩm`, 2, res.stock);
+        if (+cart.quantity + +data.quantity > stock) {
+          return errorResponse(`Chỉ còn ${stock} sản phẩm`, 2, stock);
         } else {
           cart.quantity += +data.quantity;
           await cart.save();
         }
       }
     } else {
-      // If the shopping cart does not exist, create a new shopping cart
-      let res = await db.ProductSize.findOne({
-        where: { id: data.sizeId },
-      });
-      if (res) {
-        // Calculate inventory quantity based on invoice and purchase order details
-        /*
-        let receiptDetail = await db.ReceiptDetail.findAll({
-          where: { sizeId: res.id },
+      if (data.quantity > stock) {
+        return errorResponse(`Chỉ còn ${stock} sản phẩm`, 2, stock);
+      } else {
+        await db.ShopCart.create({
+          userId: data.userId,
+          sizeId: data.sizeId,
+          quantity: data.quantity,
+          statusId: 0,
         });
-        let orderDetail = await db.OrderDetail.findAll({
-          where: { productId: res.id },
-        });
-        let quantity = 0;
-        for (let j = 0; j < receiptDetail.length; j++) {
-          quantity += receiptDetail[j].quantity;
-        }
-        for (let k = 0; k < orderDetail.length; k++) {
-          let order = await db.Order.findOne({
-            where: { id: orderDetail[k].orderId },
-          });
-          if (order.statusId != "S7") {
-            quantity -= orderDetail[k].quantity;
-          }
-        }
-        res.stock = quantity;
-        */
-
-        // Check inventory and add new items to cart
-        if (data.quantity > res.stock) {
-          return errorResponse(`Chỉ còn ${res.stock} sản phẩm`, 2, res.stock);
-        } else {
-          await db.ShopCart.create({
-            userId: data.userId,
-            sizeId: data.sizeId,
-            quantity: data.quantity,
-            statusId: 0,
-          });
-        }
       }
     }
-
     return successResponse("Add items into shop cart");
   } catch (error) {
     console.error("Error in addShopCart:", error);
-    return errorResponse("Failed to add shop cart");
+    return errorResponse(error.message);
   }
 };
 
@@ -118,16 +77,13 @@ const getShopCartByUserId = async (userId) => {
     if (!userId) {
       return errorResponse("Missing required parameter userId!");
     }
-
     const shopCartItems = await db.ShopCart.findAll({
       where: { userId: userId, statusId: 0 },
       raw: true,
     });
-
     if (!shopCartItems || shopCartItems.length === 0) {
       return errorResponse("No shop cart items found for the user!");
     }
-
     const data = [];
     for (const item of shopCartItems) {
       const productDetail = await db.ProductDetail.findOne({
@@ -136,13 +92,14 @@ const getShopCartByUserId = async (userId) => {
           {
             model: db.Product,
             as: "productData",
-            include: [
-              { model: db.AllCode, as: "categoryData" },
-              { model: db.AllCode, as: "brandData" },
-            ],
+            attributes: ["name"],
           },
-          { model: db.ProductImage, as: "productImageData" },
-          { model: db.ProductSize, as: "sizeData" },
+          {
+            model: db.ProductImage,
+            as: "productImageData",
+            attributes: ["image"],
+          },
+          { model: db.ProductSize, as: "sizeData", attributes: ["sizeId"] },
         ],
         raw: true,
       });
@@ -150,29 +107,34 @@ const getShopCartByUserId = async (userId) => {
       if (!productDetail) {
         continue;
       }
-
       const productImages = productDetail.productImageData;
       if (productImages) {
         const productImages = productImages.map(
           (productImage) => productImage.image
         );
       }
-
       data.push({
-        ...item,
-        productDetail,
-        productImages: productImages,
+        id: item.id,
+        userId: item.userId,
+        sizeId: item.sizeId,
+        sideData: productDetail["sizeData.sizeId"],
+        quantity: item.quantity,
+        productId: productDetail.productId,
+        name: productDetail["productData.name"],
+        image: productDetail["productImageData.image"],
+        color: productDetail.color,
+        originalPrice: productDetail.originalPrice,
+        discountPrice: productDetail.discountPrice,
       });
     }
-
     return {
       result: data,
       statusCode: 200,
-      errors: [`Retrieved items userId = ${userId} successfully!`],
+      errors: [`Get items with userId = ${userId} successfully!`],
     };
   } catch (error) {
     console.error("Error in getShopCartByUserId:", error);
-    return errorResponse("Failed to get shop cart by user ID");
+    return errorResponse(error.message);
   }
 };
 
